14a9551cb72ada2c1e8dfae4b8e63cbc845c8ac3
Hamid Ghorashi <hamidghorashi@hyperoffice.com>
2021-08-04 01:38:02 +0430

    Redirect all web views to home page

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/{any}', function ($any) {
+    return redirect()->route('home');
+})->where('any', '.*');
